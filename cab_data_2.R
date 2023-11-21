898e7f16c32ef3c3b8d84d8f07857e85a0b64d45
umitatlamaz <umitatlamaz@gmail.com>
2023-11-21 10:15:23 +0300

    add ch8 and ch9

--- cab_data_2.R
+++ cab_data_2.R
@@ -1,6 +1,8 @@
-cab_intercept <- 7.0
-cab_slope_distance <- 6
-cab_slope_nbridges <- 13
+library(tidyverse)
+
+cab_intercept <- 20.0
+cab_slope_distance <- 14
+cab_slope_nbridges <- 16
 cab_bridge <- 10
 rint_unif <- function(n, min, max) { 
   runif(n=n, min=min-0.499, max=max+0.499) %>% round() 
@@ -9,7 +11,7 @@ rint_unif <- function(n, min, max) {
 set.seed(42)
 
 n = 1000
-hidden_cost <- rnorm(1000, mean=7.5, sd=4.5)
+hidden_cost <- rnorm(1000, mean=20, sd=15)
 cab_fares_2 <- data.frame( distance_km = rint_unif(n, min=10, max=50),
                          n_bridges = rint_unif(n, min=0, max=10)
 )
@@ -22,3 +24,9 @@ cab_fares_2 <- within(cab_fares_2,
 
 
 cab_fares_2 <- mutate(cab_fares_2, taxi_fare_2 = round(taxi_fare_2+hidden_cost,2))
+
+
+cab_fares_2
+
+
+write.csv(cab_fares_2, file = 'data/taxi_fares_2023.csv', row.names = FALSE)
